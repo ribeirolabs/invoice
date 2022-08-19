497e0d0638621070111c68e2adcb4e148f3748b9
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-08-19 12:00:13 -0300

    feat: setup daisyui and coming soon page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="dark">
+      <Head />
+      <link rel="icon" href="/favicon.ico" />
+      <body className="prose max-w-full">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

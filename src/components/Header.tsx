28b3d6f862c3d2ffab723e416f74b11cb9930947
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-08-30 14:54:54 -0300

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <AppHeader appName="invoice">
+    <AppHeader>
       <Link href="/generate">
         <a className="btn btn-sm btn-primary btn-outline">Generate</a>
       </Link>
